dc699786e1dd5b792aa58f8b3cfb01e9257fbeb3
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2024-05-27 21:35:37 +0200

    FEAT:Created roles

---
+++ src/main/java/com/cossinest/homes/service/validator/UserRoleService.java
@@ -0,0 +1,29 @@
+package com.cossinest.homes.service.validator;
+
+import com.cossinest.homes.domain.concretes.user.UserRole;
+import com.cossinest.homes.domain.enums.RoleType;
+import com.cossinest.homes.payload.messages.ErrorMessages;
+import com.cossinest.homes.repository.user.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserRoleService {
+
+    private final UserRoleRepository userRoleRepository;
+
+    public UserRole getUserRole(RoleType roleType) throws Throwable {
+        return (UserRole) userRoleRepository.findByEnumRoleEquals(roleType).orElseThrow(
+                ()-> new ResolutionException(ErrorMessages.ROLE_NOT_FOUND)
+        );
+    }
+
+
+    public List<UserRole> getAllUserRoles(){
+        return userRoleRepository.findAll();
+    }
+}
