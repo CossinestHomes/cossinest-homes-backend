375b99e915fa3b1af966b74caf4032927cf3f390
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-06-06 17:03:58 +0300

    FEAT: Advert Type is added

---
+++ src/main/java/com/cossinest/homes/domain/concretes/business/AdvertType.java
@@ -0,0 +1,33 @@
+package com.cossinest.homes.domain.concretes.business;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "advert_type")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AdvertType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2 , max = 30, message = "Min 2 chars and Max 30 chars")
+    private String title;
+
+    private Boolean builtIn;
+
+//    @OneToMany
+//    private List<Advert> advertList;
+
+}
