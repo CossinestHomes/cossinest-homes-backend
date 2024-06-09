6f33f1935f3d0ff0f021b8a07e36b39492f06461
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2024-06-09 14:53:09 +0200

    FEAT:fsvoritesService created

--- src/main/java/com/cossinest/homes/repository/business/FavoritesRepository.java
+++ src/main/java/com/cossinest/homes/repository/business/FavoritesRepository.java
@@ -1,12 +1,20 @@
 package com.cossinest.homes.repository.business;
 
 import com.cossinest.homes.domain.concretes.business.Favorites;
+import com.cossinest.homes.payload.response.user.AuthenticatedUsersResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FavoritesRepository extends JpaRepository<Favorites,Long> {
 
-    List<Favorites> findByUserId(int userId);
+    List<Favorites> findByUserId(AuthenticatedUsersResponse userId);
+
+    @Query("SELECT f FROM Favorites f WHERE f.userId = :user_id")
+    List<Favorites> findFavorites(@Param(value = "user_id") Long id);
+
+
 
 }
