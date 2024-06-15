b79763b7a7c309de8db234a17e31c2f28031167c
ERRAYN <era74200@gmail.com>
2024-06-15 23:24:52 +0200

    FEAT:15.06.24 V1

--- src/main/java/com/cossinest/homes/contactMessage/entity/ContactMessage.java
+++ src/main/java/com/cossinest/homes/contactMessage/entity/ContactMessage.java
@@ -1,11 +1,9 @@
 package com.cossinest.homes.contactMessage.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -48,5 +46,7 @@ public class ContactMessage {
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm" , timezone = "US")
     private LocalDateTime create_at;
+
+
 
 }
