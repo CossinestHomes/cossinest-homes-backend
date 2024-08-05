b041ed29aedf05089211a9a73809be6cf67a5388
Eda Sarıkaya <edassarkaya@outlook.com>
2024-08-05 09:52:05 +0100

    city-country

--- src/main/java/com/cossinest/homes/controller/business/CityController.java
+++ src/main/java/com/cossinest/homes/controller/business/CityController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class CityController {
     @Autowired
     private  CityService cityService;
 
-    @GetMapping("/getAll") //http://localhost:8080/cities/getAll
+    @GetMapping("/getAll") //http://localhost:8080/city/getAll
     public ResponseMessage<List<City>> getAllCity(){
         List<City> cityList =cityService.getAllCity();
         return ResponseMessage.<List<City>>builder()
@@ -32,5 +33,14 @@ public class CityController {
                 .object(cityList)
                 .build();
 }
+    @GetMapping("/getByCity/{countryId}") //http://localhost:8080/city/getByCity/1
+    public ResponseMessage<List<City>> getByCity(@PathVariable Long countryId){
+        List<City> cityList =cityService.getByCity(countryId);
+        return ResponseMessage.<List<City>>builder()
+                .status(HttpStatus.OK)
+                .object(cityList)
+                .build();
+    }
+
 
 }
--- src/main/java/com/cossinest/homes/repository/business/CityRepository.java
+++ src/main/java/com/cossinest/homes/repository/business/CityRepository.java
@@ -3,6 +3,7 @@ package com.cossinest.homes.repository.business;
 import com.cossinest.homes.domain.concretes.business.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,7 @@ public interface CityRepository extends JpaRepository<City,Long> {
 
     @Query("SELECT COUNT(c) FROM City c")
     int countAllCities();
+
+    @Query("SELECT c FROM City c WHERE c.country.id=:countryId")
+    List<City> getByCity(@Param("countryId") Long countryId);
 }
