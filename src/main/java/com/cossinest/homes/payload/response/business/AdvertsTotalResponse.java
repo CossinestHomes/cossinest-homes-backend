dcd219316b0829b8b2754c762768d7e0006b8a51
ata324 <mavican324@gmail.com>
2024-08-16 22:27:39 +0200

    update

---
+++ src/main/java/com/cossinest/homes/payload/response/business/AdvertsTotalResponse.java
@@ -0,0 +1,14 @@
+package com.cossinest.homes.payload.response.business;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class AdvertsTotalResponse {
+
+    private Integer advertsTotal;
+}
