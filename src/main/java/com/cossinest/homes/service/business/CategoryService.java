c2dd18b1997207dd26766ea5bedaa11fa8b0c0aa
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-08-18 16:51:25 +0300

    update

--- src/main/java/com/cossinest/homes/service/business/CategoryService.java
+++ src/main/java/com/cossinest/homes/service/business/CategoryService.java
@@ -287,6 +287,25 @@ public class CategoryService {
         categoryRepository.deleteByBuiltIn(false);
     }*/
 
+    public void generateCategory(){
+        if (categoryRepository.findAll().isEmpty()) {
+            List<Category> categories = List.of(
+                    new Category(1L, "Müstakil Ev", "ev_icon", true, 0, "mustakil-ev", true),
+                    new Category(2L, "Apartman Dairesi", "dairesi_icon", true, 0, "apartman-dairesi", true),
+                    new Category(3L, "Ofis", "ofis_icon", true, 0, "kelepir-ofis", true),
+                    new Category(4L, "Villa", "villa_icon", true, 0, "kelepir-villa", true),
+                    new Category(5L, "Arsa", "arsa_icon", true, 0, "kelepir-arsa", true)
+            );
+
+            for (Category category : categories) {
+                category.setCreatedAt(LocalDateTime.now());
+                category.setUpdatedAt(LocalDateTime.now());
+            }
+
+            categoryRepository.saveAll(categories);
+        }
+    }
+
 
     }
 
