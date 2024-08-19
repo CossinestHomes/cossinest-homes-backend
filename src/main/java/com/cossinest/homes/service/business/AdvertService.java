a119abdacf4abbbb4be1dfa057ebb9a18253350e
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-08-20 01:56:26 +0300

    fixed

--- src/main/java/com/cossinest/homes/service/business/AdvertService.java
+++ src/main/java/com/cossinest/homes/service/business/AdvertService.java
@@ -236,6 +236,11 @@ public class AdvertService {
         if(advert.getBuiltIn()){
             throw new ResourceNotFoundException(ErrorMessages.THIS_ADVERT_DOES_NOT_UPDATE);
         }
+
+        if (!advert.getUser().getId().equals(user.getId())) {
+            throw new ResourceNotFoundException(ErrorMessages.ADVERT_NOT_FOUND_FOR_USER);
+        }
+
         Map<String, Object> detailsMap = new HashMap<>();
         getAdvertDetails(advertRequest,request,detailsMap);
 
